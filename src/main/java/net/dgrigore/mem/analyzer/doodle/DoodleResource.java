fb34c29f382226d4a2a4a336437374d0f22f81ec
Daniel Grigore <daniel.grigore@gmail.com>
2018-03-26 22:34:44 +0300

    Added more options to doodle methods.

--- src/main/java/net/dgrigore/mem/analyzer/doodle/DoodleResource.java
+++ src/main/java/net/dgrigore/mem/analyzer/doodle/DoodleResource.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,10 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DoodleResource {
 
     private static final Logger LOG = LoggerFactory.getLogger(DoodleResource.class);
-
-    private static final int COUNT = 400;
-
-    private static final int PAGE_SIZE = 50;
 
     private final DoodleDao dao;
 
@@ -34,14 +31,14 @@ public class DoodleResource {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public void load() throws IOException {
+    public void load(@RequestParam(name = "size", defaultValue = "100") int size) throws IOException {
         InputStream in = this.getClass().getClassLoader().getResourceAsStream("img.jpg");
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         FileCopyUtils.copy(in, out);
         LOG.info("Loaded {} bytes.", out.toByteArray().length);
 
         List<Doodle> doodleList = new ArrayList<>();
-        for (int i = 0; i < COUNT; i++) {
+        for (int i = 0; i < size; i++) {
             Doodle d = new Doodle();
             d.setData(out.toByteArray());
             doodleList.add(d);
@@ -50,12 +47,15 @@ public class DoodleResource {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<Doodle> get() {
-        long count = this.dao.count();
-        long pages = count / PAGE_SIZE;
+    public List<Doodle> get(@RequestParam(name = "size", defaultValue = "100") int size,
+            @RequestParam(name = "count", defaultValue = "100") int count) {
+        long pages = count / size;
+        if (count % pages != 0) {
+            pages += 1;
+        }
         LOG.info("Found {} doodles and {} pages", count, pages);
         for (int page = 0; page < pages; page++) {
-            List<Doodle> doodles = this.dao.find(page * PAGE_SIZE, PAGE_SIZE);
+            List<Doodle> doodles = this.dao.find(page * size, size);
             LOG.info("Loaded page {} ", page);
             this.dao.detach(doodles);
         }
